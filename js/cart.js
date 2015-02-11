47a078fc327beae9e64e96a149124b3cb5d2810f
Exal Matuz <iscematuz@gmail.com>
2015-02-10 23:55:38 -0600

    js

--- js/cart.js
+++ js/cart.js
@@ -1,13 +1,27 @@
 'use strict';
 
+/**
+* ng-Shop v0.0.1 Israel Parra - modulo que cumple el proceso de compra con angularjs
+* @link - http://uno-de-piera.com/carrito-de-compras-con-paypal-en-angularjs
+*/
+
 var shop = angular.module('ng-shop', []);
 
+//nuestra factoria se llamará $shop, inyectamos $rootScope
+//devuleve un objeto con toda la funcionalidad que debe tener un carrito
 shop.factory('$shop', ['$rootScope', function($rootScope)
 {
-	$rootScope.udpShopContent = [],
-
-	$rootScope.udpShopTotalPrice = 0,
-
+	/**
+    * @var array con el contenido del carrito
+    */
+	$rootScope.udpShopContent = [];
+	/**
+    * @var float con el precio total del carrito
+    */
+	$rootScope.udpShopTotalPrice = 0;
+	/**
+    * @var integer con el número de artículos del carrito
+    */
 	$rootScope.udpShopTotalProducts = 0;
 
 	return{
@@ -22,7 +36,50 @@ shop.factory('$shop', ['$rootScope', function($rootScope)
 			if(product.qty <= 0){
 				throw new Error("La cantidad añadida debe ser mayor de 0");
 			}
-			if()
+			if(this.isInteger(product.qty) === false){
+				throw new Error("La cantidad del producto debe ser un numero entero");
+			},
+
+		/**
+        * @desc - comprueba si el número pasado es un entero
+        * @return - bool
+        */
+			isInteger: function(n)
+			{
+				if(n % 1 === 0)
+					return true;
+				else
+					return false;
+			},
+
+		/*
+        * @desc - añade nuevos productos al carrito
+        * @param - array con los datos del producto
+        * @return - mixed
+        */
+			add: function(producto)
+			{
+				try{
+					//comprobamos si el producto cumple los requisitos
+					this.minimRequeriments(producto);
+					//si el producto existe le actualizamos la cantidad
+					if(this.checkExistProduct(producto, $rootScope.udpShopContent) === true){
+						$rootScope.udpShopTotalPrice += parseFloat(producto.price * producto.qty, 10);
+						$rootScope.udpShopTotalProducts += producto.qty;
+						return {"msg":"updated"};
+					}
+					//en otro caso, lo añadimos al carrito
+					else{
+						$rootScope.udpShopTotalPrice += parseFloat(producto.price * producto.qty, 10);
+						$rootScope.udpShopTotalProducts += producto.qty;
+						$rootScope.udpShopContent.push(producto);
+						return{"msg":"insert"};
+					}
+				}
+				catch(error){
+					alert("Error" + error);
+				}
+			}
 		}
 	};
 }]);
