8ff6b1da7e8297bf9bcf2d180d384151ed9dfba9
Exal Matuz <iscematuz@gmail.com>
2015-02-10 23:11:50 -0600

    js

--- js/cart.js
+++ js/cart.js
@@ -9,4 +9,20 @@ shop.factory('$shop', ['$rootScope', function($rootScope)
 	$rootScope.udpShopTotalPrice = 0,
 
 	$rootScope.udpShopTotalProducts = 0;
+
+	return{
+		minimRequeriments: function(product)
+		{
+			if(!product.qty || !product.price || !product.id){
+				throw new Error("Los campos qty, price y id son necesarios");
+			}
+			if(isNaN(product.qty) || isNaN(product.price) || isNaN(product.id)){
+				throw new Error("Los campos qty, price y id deben ser numericos");
+			}
+			if(product.qty <= 0){
+				throw new Error("La cantidad añadida debe ser mayor de 0");
+			}
+			if()
+		}
+	};
 }]);
